36a3d7f5e86a630641b2b61bc42732067ddcefb2
akashkhunt <akash.mercer@gmail.com>
2023-06-10 14:35:32 +0530

    `AndroidLibraryConventionPlugin` & `AndroidLibraryComposeConventionPlugin` implemented

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,3 +1,22 @@
 plugins {
     `kotlin-dsl`
 }
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.spotless.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidLibrary") {
+            id = "android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("androidLibraryCompose") {
+            id = "android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+    }
+}
---
+++ build-logic/convention/src/main/kotlin/KotlinAndroid.kt
@@ -0,0 +1,58 @@
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.plugins.ExtensionAware
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+
+/**
+ * Configure base Kotlin with Android options
+ */
+internal fun Project.configureKotlinAndroid(
+	commonExtension: CommonExtension<*, *, *, *, *>,
+) {
+	commonExtension.apply {
+		compileSdk = 33
+
+		defaultConfig {
+			minSdk = 26
+
+			testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		}
+
+		compileOptions {
+			sourceCompatibility = JavaVersion.VERSION_17
+			targetCompatibility = JavaVersion.VERSION_17
+			isCoreLibraryDesugaringEnabled = true
+		}
+
+		kotlinOptions {
+			// Treat all Kotlin warnings as errors (disabled by default)
+			allWarningsAsErrors = properties["warningsAsErrors"] as? Boolean ?: false
+
+			freeCompilerArgs = freeCompilerArgs + listOf(
+				"-opt-in=kotlin.RequiresOptIn",
+				// Enable experimental coroutines APIs, including Flow
+				"-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+				"-opt-in=kotlinx.coroutines.FlowPreview",
+				"-opt-in=kotlin.Experimental",
+				// Enable experimental kotlinx serialization APIs
+				"-opt-in=kotlinx.serialization.ExperimentalSerializationApi",
+			)
+
+			jvmTarget = JavaVersion.VERSION_17.toString()
+		}
+	}
+
+	val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+	dependencies {
+		add("coreLibraryDesugaring", libs.findLibrary("android.desugarJdkLibs").get())
+	}
+}
+
+fun CommonExtension<*, *, *, *, *>.kotlinOptions(block: KotlinJvmOptions.() -> Unit) {
+	(this as ExtensionAware).extensions.configure("kotlinOptions", block)
+}
